9c73c7bf55146cec53bd6ed62c36497f2325f250
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-09-29 15:11:49 +0200

    for writing data as JSON

---
+++ src/main/java/com/ride2go/ridetogtfsconverter/conversion/JSONConverter.java
@@ -0,0 +1,26 @@
+package com.ride2go.ridetogtfsconverter.conversion;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class JSONConverter {
+
+	private static final Logger LOG = LoggerFactory.getLogger(JSONConverter.class);
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	public String toJSONString(final Object o) {
+		try {
+			return objectMapper.writeValueAsString(o);
+		} catch (JsonProcessingException e) {
+			LOG.warn("JSON processing problem: " + e.getMessage());
+		}
+		return "";
+	}
+}
