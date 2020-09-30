94300dcb32534aab51a51b376d5ffae22eacd1ac
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-09-30 11:32:26 +0200

    configure WebClient to avoid Exception with OSRM routing: org.springframework.core.io.buffer.DataBufferLimitException: Exceeded limit on max bytes to buffer : 262144

---
+++ src/main/java/com/ride2go/ridetogtfsconverter/configuration/SystemConfiguration.java
@@ -0,0 +1,21 @@
+package com.ride2go.ridetogtfsconverter.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class SystemConfiguration {
+
+	@Bean
+	public WebClient getWebClientBuilder() {
+		return WebClient.builder()
+				.exchangeStrategies(
+						ExchangeStrategies.builder()
+							.codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
+							.build()
+				)
+				.build();
+	}
+}
