eb9c924b5512ef123e38f6a4ca965a30bce26f9e
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-10-30 11:48:22 +0100

    add entries to GTFS files instead of overwriting them

--- src/main/java/com/ride2go/ridetogtfsconverter/gtfs/WriterService.java
+++ src/main/java/com/ride2go/ridetogtfsconverter/gtfs/WriterService.java
@@ -1,10 +1,13 @@
 package com.ride2go.ridetogtfsconverter.gtfs;
 
+import java.io.File;
 import java.util.List;
 
 import com.ride2go.ridetogtfsconverter.model.item.Offer;
 
 public interface WriterService {
 
-	public void writeAsGTFS(final List<Offer> offers, final String directory);
+	public void writeProviderInfoAsGTFS(final File directory);
+
+	public void writeOfferDataAsGTFS(final List<Offer> offers, final File directory);
 }
