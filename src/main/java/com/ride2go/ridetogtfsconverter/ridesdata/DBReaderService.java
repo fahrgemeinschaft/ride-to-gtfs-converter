3ed1e95e35d99bb636a1d68960f068441abc93ac
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-11-12 12:10:30 +0100

    new db reading service methods using pagination and concurrency

--- src/main/java/com/ride2go/ridetogtfsconverter/ridesdata/DBReaderService.java
+++ src/main/java/com/ride2go/ridetogtfsconverter/ridesdata/DBReaderService.java
@@ -4,10 +4,14 @@ import static com.ride2go.ridetogtfsconverter.util.DateAndTimeHandler.YESTERDAY;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.ride2go.ridetogtfsconverter.conversion.OfferConverter;
@@ -41,6 +45,25 @@ public class DBReaderService implements ReaderService {
 		return offers;
 	}
 
+	@Async
+	public CompletableFuture<List<Offer>> getOffersByUserIdAsync(final String userId) {
+		return CompletableFuture.completedFuture(getOffersByUserId(userId));
+	}
+
+	public List<Offer> getOfferPage(final Pageable page) {
+		List<EntityTrip> trips = getValidRelevantAndOngoingOffers(page);
+		List<Offer> offers = offerConverter.fromTripToOffer(trips);
+		if (size > 0) {
+			LOG.info("Using {} valid, relevant and ongoing user offers having all required fields", trips.size());
+		}
+		return offers;
+	}
+
+	@Async
+	public CompletableFuture<List<Offer>> getOfferPageAsync(final Pageable page) {
+		return CompletableFuture.completedFuture(getOfferPage(page));
+	}
+
 	private List<EntityTrip> getValidRelevantAndOngoingUserOffers(final String userId) {
 		List<EntityTrip> trips = getOfferList(userId);
 		size = trips.size();
@@ -50,12 +73,37 @@ public class DBReaderService implements ReaderService {
 		return trips;
 	}
 
+	private List<EntityTrip> getValidRelevantAndOngoingOffers(final Pageable page) {
+		List<EntityTrip> trips = getOfferList(page);
+		size = trips.size();
+		LOG.info("Found {} trips in the database for page {}", size, page.getPageNumber());
+		tripValidator.validTrips(trips);
+		ongoingTrips(trips);
+		return trips;
+	}
+
+	private static final String TRIPTYPE_OFFER = "offer";
+
+	private static final Integer RELEVANCE_SEARCHABLE = 10;
+
 	private List<EntityTrip> getOfferList(final String userId) {
-		List<EntityTrip> offers = tripRepository.findByUserIdAndTriptypeAndRelevance(userId, "offer", 10);
-		if (offers == null) {
-			return new ArrayList<>();
+		List<EntityTrip> offers = null;
+		try {
+			offers = tripRepository.findByUserIdAndTriptypeAndRelevance(userId, TRIPTYPE_OFFER, RELEVANCE_SEARCHABLE);
+		} catch (JpaObjectRetrievalFailureException e) {
+			LOG.info("JPA object retrieval failure when getting user trips: " + e.getMessage());
 		}
-		return offers;
+		return (offers == null) ? new ArrayList<>() : offers;
+	}
+
+	private List<EntityTrip> getOfferList(final Pageable page) {
+		List<EntityTrip> offers = null;
+		try {
+			offers = tripRepository.findByTriptypeAndRelevance(TRIPTYPE_OFFER, RELEVANCE_SEARCHABLE, page);
+		} catch (JpaObjectRetrievalFailureException e) {
+			LOG.error("JPA object retrieval failure when getting trip page: " + e.getMessage());
+		}
+		return (offers == null) ? new ArrayList<>() : offers;
 	}
 
 	private void ongoingTrips(List<EntityTrip> trips) {
