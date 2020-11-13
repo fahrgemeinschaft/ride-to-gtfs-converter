38a1b5166989e3468691a6d3cac9b7d69a038e8c
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-11-13 11:48:58 +0100

    add constants for feed times

--- src/main/java/com/ride2go/ridetogtfsconverter/gtfs/OBAWriterParameter.java
+++ src/main/java/com/ride2go/ridetogtfsconverter/gtfs/OBAWriterParameter.java
@@ -1,5 +1,12 @@
 package com.ride2go.ridetogtfsconverter.gtfs;
 
+import static com.ride2go.ridetogtfsconverter.util.DateAndTimeHandler.ONE_MONTH_FROM_TODAY;
+import static com.ride2go.ridetogtfsconverter.util.DateAndTimeHandler.TODAY;
+
+import java.time.LocalDate;
+
+import org.onebusaway.gtfs.model.calendar.ServiceDate;
+
 public class OBAWriterParameter {
 
 	private OBAWriterParameter() {
@@ -12,4 +19,16 @@ public class OBAWriterParameter {
 	protected static final int EXACT_TIMEPOINT = 1;
 
 	protected static final int APPROXIMATE_TIMEPOINT = 0;
+
+	public static final LocalDate FEED_START_DATE = TODAY;
+
+	public static final LocalDate FEED_END_DATE = ONE_MONTH_FROM_TODAY;
+
+	protected static final ServiceDate OBA_FEED_START_DATE = getByDate(FEED_START_DATE);
+
+	protected static final ServiceDate OBA_FEED_END_DATE = getByDate(FEED_END_DATE);
+
+	protected static ServiceDate getByDate(LocalDate date) {
+		return new ServiceDate(date.getYear(), date.getMonthValue(), date.getDayOfMonth());
+	}
 }
