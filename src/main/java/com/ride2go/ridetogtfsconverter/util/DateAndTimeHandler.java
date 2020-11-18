9d40cdab86f3f65adfb27d9bda0ffd6eb9f13e6a
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-11-18 11:16:26 +0100

    update date and time constants

--- src/main/java/com/ride2go/ridetogtfsconverter/util/DateAndTimeHandler.java
+++ src/main/java/com/ride2go/ridetogtfsconverter/util/DateAndTimeHandler.java
@@ -9,11 +9,13 @@ public class DateAndTimeHandler {
 	private DateAndTimeHandler() {
 	}
 
+	public static final String TIME_ZONE_BERLIN = "Europe/Berlin";
+
+	public static final ZoneId TIME_ZONE_ID_BERLIN = ZoneId.of(TIME_ZONE_BERLIN);
+
 	public static final DateTimeFormatter DATA_TIME_FORMATTER = DateTimeFormatter.ofPattern("Hmm");
 
-	public static final LocalDate TODAY = LocalDate.now(ZoneId.of("Europe/Berlin"));
-
-	public static final LocalDate YESTERDAY = TODAY.minusDays(1);
+	public static final LocalDate TODAY = LocalDate.now(TIME_ZONE_ID_BERLIN);
 
 	public static final LocalDate ONE_MONTH_FROM_TODAY = TODAY.plusMonths(1);
 
