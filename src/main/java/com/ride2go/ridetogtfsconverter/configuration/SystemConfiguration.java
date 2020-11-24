12335e301b09a830ee8a8f55a2f8340340c242b2
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-11-24 13:44:55 +0100

    move concurrency bean, add circuit breaker bean

--- src/main/java/com/ride2go/ridetogtfsconverter/configuration/SystemConfiguration.java
+++ src/main/java/com/ride2go/ridetogtfsconverter/configuration/SystemConfiguration.java
@@ -1,12 +1,35 @@
 package com.ride2go.ridetogtfsconverter.configuration;
 
+import java.time.Duration;
+import java.util.concurrent.Executor;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+
 @Configuration
 public class SystemConfiguration {
+
+	public static final int AMOUNT_OF_THREADS = 10;
+
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(AMOUNT_OF_THREADS);
+		executor.setMaxPoolSize(AMOUNT_OF_THREADS);
+		executor.setQueueCapacity(AMOUNT_OF_THREADS);
+		executor.setThreadNamePrefix("dbLookup-");
+		executor.initialize();
+		return executor;
+	}
 
 	@Bean
 	public WebClient getWebClientBuilder() {
@@ -17,5 +40,17 @@ public class SystemConfiguration {
 							.build()
 				)
 				.build();
+	}
+
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.ofDefaults();
+		TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
+				.timeoutDuration(Duration.ofSeconds(10))
+				.build();
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(circuitBreakerConfig)
+				.timeLimiterConfig(timeLimiterConfig)
+				.build());
 	}
 }
