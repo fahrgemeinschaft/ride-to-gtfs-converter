706b9ef684d09ac84d6d7cd7a5f0612781af9d46
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-11-25 14:48:23 +0100

    scan new configuration properties

--- src/main/java/com/ride2go/ridetogtfsconverter/RideToGtfsConverterApplication.java
+++ src/main/java/com/ride2go/ridetogtfsconverter/RideToGtfsConverterApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("com.ride2go.ridetogtfsconverter.configuration")
 @ComponentScan("com.ride2go.ridetogtfsconverter")
 @EntityScan("com.ride2go.ridetogtfsconverter.model.data.ride")
 @EnableJpaRepositories("com.ride2go.ridetogtfsconverter.repository")
