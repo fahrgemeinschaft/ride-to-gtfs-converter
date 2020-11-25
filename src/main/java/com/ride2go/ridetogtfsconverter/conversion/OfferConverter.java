59e8f6249d0f1a78bbabc7d895a97fa7171cc479
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-11-25 14:39:57 +0100

    check place double too for stop time

--- src/main/java/com/ride2go/ridetogtfsconverter/conversion/OfferConverter.java
+++ src/main/java/com/ride2go/ridetogtfsconverter/conversion/OfferConverter.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import com.ride2go.ridetogtfsconverter.model.data.ride.EntityReoccurs;
+import com.ride2go.ridetogtfsconverter.model.data.ride.EntityRouting;
 import com.ride2go.ridetogtfsconverter.model.data.ride.EntityRoutingPlace;
 import com.ride2go.ridetogtfsconverter.model.data.ride.EntityTrip;
 import com.ride2go.ridetogtfsconverter.model.item.GeoCoordinates;
@@ -25,60 +27,81 @@ public class OfferConverter {
 
 	public List<Offer> fromTripToOffer(final List<EntityTrip> trips) {
 		List<Offer> offers = new ArrayList<>();
-		int last;
 		for (EntityTrip trip : trips) {
 			Offer offer = new Offer();
 			offer.setId(trip.getTripId());
 			offer.setStartDate(trip.getStartdate());
-			last = trip.getRoutings().size() - 1;
-			Place originPlace = getPlace(trip.getRoutings().get(last).getOrigin());
-			originPlace.setTimeInSeconds(trip.getStarttime().toSecondOfDay());
-			previousTimeInSeconds = originPlace.getTimeInSeconds();
-			List<Place> places = new ArrayList<>();
-			places.add(originPlace);
-			if (trip.getRoutings().size() > 1) {
-				for (int i = 0; i < trip.getRoutings().size() - 2; i++) {
-					Place intermediatePlace = getPlace(trip.getRoutings().get(i).getDestination());
-					places.add(intermediatePlace);
-				}
-			}
-			Place destinationPlace = getPlace(trip.getRoutings().get(last).getDestination());
-			places.add(destinationPlace);
-			offer.setPlaces(places);
-			if (trip.getReoccurs().doesReoccur()) {
-				Recurring recurring = new Recurring();
-				recurring.setMonday(trip.getReoccurs().getMo());
-				recurring.setTuesday(trip.getReoccurs().getTu());
-				recurring.setWednesday(trip.getReoccurs().getWe());
-				recurring.setThursday(trip.getReoccurs().getTh());
-				recurring.setFriday(trip.getReoccurs().getFr());
-				recurring.setSaturday(trip.getReoccurs().getSa());
-				recurring.setSunday(trip.getReoccurs().getSu());
-				offer.setRecurring(recurring);
-			}
+			offer.setPlaces(getPlaces(trip));
+			offer.setRecurring(getRecurring(trip));
 			offer.setMissingreoccurs(trip.getMissingreoccurs());
 			offers.add(offer);
 		}
 		return offers;
 	}
 
-	private Place getPlace(final EntityRoutingPlace entityRoutingPlace) {
+	private List<Place> getPlaces(final EntityTrip trip) {
+		List<Place> places = new ArrayList<>();
+		List<EntityRouting> routings = trip.getRoutings();
+		int last = routings.size() - 1;
+		Place originPlace = getPlace(routings.get(last).getOrigin(), null);
+		originPlace.setTimeInSeconds(trip.getStarttime().toSecondOfDay());
+		previousTimeInSeconds = originPlace.getTimeInSeconds();
+		places.add(originPlace);
+		if (routings.size() > 1) {
+			for (int i = 0; i < routings.size() - 2; i++) {
+				Place intermediatePlace = getPlace(routings.get(i).getDestination(),
+						routings.get(i + 1).getOrigin());
+				places.add(intermediatePlace);
+			}
+		}
+		Place destinationPlace = getPlace(routings.get(last).getDestination(),
+				routings.get(last - 1).getDestination());
+		places.add(destinationPlace);
+		return places;
+	}
+
+	private Recurring getRecurring(final EntityTrip trip) {
+		EntityReoccurs reoccurs = trip.getReoccurs();
+		if (reoccurs.doesReoccur()) {
+			Recurring recurring = new Recurring();
+			recurring.setMonday(reoccurs.getMo());
+			recurring.setTuesday(reoccurs.getTu());
+			recurring.setWednesday(reoccurs.getWe());
+			recurring.setThursday(reoccurs.getTh());
+			recurring.setFriday(reoccurs.getFr());
+			recurring.setSaturday(reoccurs.getSa());
+			recurring.setSunday(reoccurs.getSu());
+			return recurring;
+		}
+		return null;
+	}
+
+	private Place getPlace(final EntityRoutingPlace entityRoutingPlace,
+			final EntityRoutingPlace entityRoutingPlaceDouble) {
 		GeoCoordinates geoCoordinates = new GeoCoordinates(entityRoutingPlace.getLat(), entityRoutingPlace.getLon());
 
 		Place place = new Place();
 		place.setId(entityRoutingPlace.getPlaceId());
 		place.setGeoCoordinates(geoCoordinates);
 		place.setAddress(entityRoutingPlace.getAddress());
+		place.setTimeInSeconds(getTimeInSeconds(entityRoutingPlace, entityRoutingPlaceDouble));
+		return place;
+	}
 
+	private Integer getTimeInSeconds(final EntityRoutingPlace entityRoutingPlace,
+			final EntityRoutingPlace entityRoutingPlaceDouble) {
 		LocalTime stoptime = entityRoutingPlace.getStoptime();
+		if (stoptime == null) {
+			stoptime = entityRoutingPlaceDouble.getStoptime();
+		}
 		if (stoptime != null) {
 			int stoptimeInSeconds = stoptime.toSecondOfDay();
 			while (stoptimeInSeconds < previousTimeInSeconds) {
 				stoptimeInSeconds += ONE_DAY_IN_SECONDS;
 			}
-			place.setTimeInSeconds(stoptimeInSeconds);
 			previousTimeInSeconds = stoptimeInSeconds;
+			return stoptimeInSeconds;
 		}
-		return place;
+		return null;
 	}
 }
