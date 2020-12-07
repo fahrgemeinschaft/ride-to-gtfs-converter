112c1392e9c194e2662b1a2dea1cd95088b51e4d
Katrin Ka <hellomynameiskatrin@gmail.com>
2020-12-07 19:45:00 +0100

    decide to have a scheduled run or only one run

--- src/main/java/com/ride2go/ridetogtfsconverter/RideToGtfsConverterApplication.java
+++ src/main/java/com/ride2go/ridetogtfsconverter/RideToGtfsConverterApplication.java
@@ -8,11 +8,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ConfigurationPropertiesScan("com.ride2go.ridetogtfsconverter.configuration")
 @ComponentScan("com.ride2go.ridetogtfsconverter")
 @EntityScan("com.ride2go.ridetogtfsconverter.model.data.ride")
@@ -22,17 +25,41 @@ public class RideToGtfsConverterApplication implements CommandLineRunner {
 
 	private static final Logger LOG = LoggerFactory.getLogger(RideToGtfsConverterApplication.class);
 
+	private static final String RUN_ONCE = "runOnce";
+
 	@Autowired
 	private RunService runService;
 
+	@Autowired
+	private ScheduledService scheduledService;
+
+	private static boolean runOnce;
+
 	public static void main(String[] args) {
-		SpringApplication.run(RideToGtfsConverterApplication.class, args)
-			.close();
-		LOG.info("done");
+		ConfigurableApplicationContext context = SpringApplication.run(RideToGtfsConverterApplication.class, args);
+		if (runOnce) {
+			context.close();
+			LOG.info("Done");
+		}
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		runService.run(args);
+		init(args);
+		if (runOnce) {
+			LOG.info("Run once");
+			runService.run();
+		} else {
+			LOG.info("Run with schedule");
+			scheduledService.enable = true;
+		}
+	}
+
+	private void init(String... args) {
+		if (args != null && args.length > 0 && args[0] != null && args[0].trim().equals(RUN_ONCE)) {
+			runOnce = true;
+		} else {
+			runOnce = false;
+		}
 	}
 }
