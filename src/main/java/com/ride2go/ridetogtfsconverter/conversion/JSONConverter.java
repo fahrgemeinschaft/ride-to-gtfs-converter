0b39897597e368f9771c1f54e12a7bc2edbf7b8c
Katrin Ka <hellomynameiskatrin@gmail.com>
2021-01-12 12:43:29 +0100

    JSON converter with new methods for pretty print and convertion from JSON string

--- src/main/java/com/ride2go/ridetogtfsconverter/conversion/JSONConverter.java
+++ src/main/java/com/ride2go/ridetogtfsconverter/conversion/JSONConverter.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
@@ -20,9 +22,35 @@ public class JSONConverter {
 			try {
 				return objectMapper.writeValueAsString(o);
 			} catch (JsonProcessingException e) {
-				LOG.warn("JSON processing problem: " + e.getMessage());
+				LOG.warn("JSON processing problem while serializing: " + e.getMessage());
 			}
 		}
 		return "";
+	}
+
+	public String toPrettyJSONString(final Object o) {
+		if (o != null) {
+			try {
+				return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(o);
+			} catch (JsonProcessingException e) {
+				LOG.warn("JSON processing problem while serializing with pretty printer: " + e.getMessage());
+			}
+		}
+		return "";
+	}
+
+	public <T> T fromJSONString(String jsonString, Class<T> returnType) {
+		if (jsonString != null && returnType != null) {
+			try {
+				return objectMapper.readValue(jsonString, returnType);
+			} catch (JsonMappingException e) {
+				LOG.warn("JSON mapping problem while deserializing: " + e.getMessage());
+				e.printStackTrace();
+			} catch (JsonProcessingException e) {
+				LOG.warn("JSON processing problem while deserializing: " + e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 }
