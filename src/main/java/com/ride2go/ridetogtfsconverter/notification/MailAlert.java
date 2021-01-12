8cf011a271cd13c7a0083f0b87e98ff6a08bcf25
Katrin Ka <hellomynameiskatrin@gmail.com>
2021-01-12 12:36:43 +0100

    alert implementation for sending notifications

---
+++ src/main/java/com/ride2go/ridetogtfsconverter/notification/MailAlert.java
@@ -0,0 +1,44 @@
+package com.ride2go.ridetogtfsconverter.notification;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailAuthenticationException;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailParseException;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailAlert implements Alert {
+
+	private static final Logger LOG = LoggerFactory.getLogger(MailAlert.class);
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@Override
+	public void send(String[] recipients, String subject, String text) {
+		try {
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setTo(recipients);
+			message.setSubject(subject);
+			message.setText(text);
+			javaMailSender.send(message);
+		} catch (MailParseException e) {
+			LOG.error("Problem while parsing the mail message: " + e.getMessage());
+			e.printStackTrace();
+		} catch (MailAuthenticationException e) {
+			LOG.error("Mail sender authentication failed: " + e.getMessage());
+			e.printStackTrace();
+		} catch (MailSendException e) {
+			LOG.error("Problem while sending the mail message: " + e.getMessage());
+			e.printStackTrace();
+		} catch (MailException e) {
+			LOG.error("Mail problem: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
