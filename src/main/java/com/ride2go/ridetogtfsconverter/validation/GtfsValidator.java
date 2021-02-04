36961654ecd5d169cd849a79ac96ac58cfeab063
Katrin Ka <hellomynameiskatrin@gmail.com>
2021-02-04 13:58:57 +0100

    also add the method to the interface

--- src/main/java/com/ride2go/ridetogtfsconverter/validation/GtfsValidator.java
+++ src/main/java/com/ride2go/ridetogtfsconverter/validation/GtfsValidator.java
@@ -3,6 +3,8 @@ package com.ride2go.ridetogtfsconverter.validation;
 public interface GtfsValidator {
 
 	public String[] getRecipients();
-	
+
+	public void setRecipients(String[] recipients);
+
 	public void check(String input, String output);
 }
