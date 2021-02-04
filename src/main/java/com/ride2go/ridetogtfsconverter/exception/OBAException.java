e4181b42fbd652e5bfbbd842581f412a05178c78
Katrin Ka <hellomynameiskatrin@gmail.com>
2021-02-04 12:42:14 +0100

    will be used to handle OneBusAway errors

---
+++ src/main/java/com/ride2go/ridetogtfsconverter/exception/OBAException.java
@@ -0,0 +1,10 @@
+package com.ride2go.ridetogtfsconverter.exception;
+
+public class OBAException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public OBAException(String message) {
+		super(message);
+	}
+}
